4a66740033a83585e317433d89d68af8ba68f587
Goddchen <goddchen@gmail.com>
2024-08-22 08:04:45 +0200

    feat: upgrade to Flutter 3.22.3 and upgrade packages (#7)
    * feat: upgrade to Flutter 3.22.3 and upgrade packages
    
    Signed-off-by: Goddchen <goddchen@gmail.com>
    
    * ci: upgrade to Flutter 3.22.3
    
    Signed-off-by: Goddchen <goddchen@gmail.com>
    
    * feat: enable windows platform
    
    Signed-off-by: Goddchen <goddchen@gmail.com>
    
    * ci: try to fix web asset loading
    
    Signed-off-by: Goddchen <goddchen@gmail.com>
    
    ---------
    
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- android/app/src/main/kotlin/de/goddchen/flutter/future_of_flutter_testing/MainActivity.kt
+++ android/app/src/main/kotlin/de/goddchen/flutter/future_of_flutter_testing/MainActivity.kt
@@ -2,5 +2,4 @@ package de.goddchen.flutter.future_of_flutter_testing
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
