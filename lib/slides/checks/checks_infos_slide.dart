dee034e68c1dcf277203508da709aee9b5016930
Goddchen <goddchen@gmail.com>
2024-08-27 09:50:33 +0200

    feat: introduce local copy of bullet list that has mainAxisSize: min (#13)
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- lib/slides/checks/checks_infos_slide.dart
+++ lib/slides/checks/checks_infos_slide.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_deck/flutter_deck.dart';
+import 'package:future_of_flutter_testing/widgets/bullet_list.dart'
+    as local_bullet_list;
 import 'package:future_of_flutter_testing/widgets/package_badge.dart';
 
 class ChecksInfosSlide extends FlutterDeckSlideWidget {
@@ -24,7 +26,7 @@ class ChecksInfosSlide extends FlutterDeckSlideWidget {
           },
           version: '0.3.0',
         ),
-        rightBuilder: (_) => FlutterDeckBulletList(
+        rightBuilder: (_) => local_bullet_list.FlutterDeckBulletList(
           items: const <String>[
             'Matching package for tests',
             'Developed by the official Dart team',
